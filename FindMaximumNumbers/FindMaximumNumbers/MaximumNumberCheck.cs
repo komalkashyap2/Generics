aa3ad221ce9903585bb7c38b8548e548fab2cfb1
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-07 13:19:33 +0530

    [KOMAL KASHYAP] find maximum of three integer number

---
+++ FindMaximumNumbers/FindMaximumNumbers/MaximumNumberCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumbers
+{
+     public class MaximumNumberCheck
+    {
+        //This is method created for maximum integer number
+        public static int maximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+                  
+            }
+            throw new Exception("firstNumber,secondNumber,thirdNumber are same");
+        }
+    }
+}
---
+++ FindMaximumNumbers/FindMaximumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumbers
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+
+            int output = MaximumNumberCheck.maximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
